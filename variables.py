25f67efe8eac4e8ce9d4675f8fd17ec1d04a8c36
ChanakaUOMIT <chanakauomfit@gmail.com>
2020-10-22 14:07:06 +0530

    initilaize

---
+++ variables.py
@@ -0,0 +1,17 @@
+# Variable declaration and Assignment
+name = "Chanaka Sampath"
+num = 7
+age = 26
+
+# Multi-Assignment within a single statement
+name, num, age = "Chanaka Sampath", 7, 26
+
+# get the output
+print(name)
+print(num)
+print(age)
+
+# output multi values
+print("Name : ", name, " his age : ", age, " and lucky number is ", num)
+print("Name : " + name + " his age : " +
+      str(age) + " and lucky number is ", str(num))
